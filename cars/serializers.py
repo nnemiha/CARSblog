36f9841a3867729d1b4ebd76069af13ce6f80699
nnemiha <suskikovsuslik9@gmail.com>
2025-10-27 20:32:47 +0200

    heizenberg

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Car
+from django.contrib.auth.models import User
 
 class CarSerializer(serializers.ModelSerializer):
     picture = serializers.SerializerMethodField()
@@ -13,3 +14,18 @@ class CarSerializer(serializers.ModelSerializer):
         if obj.picture:
             return obj.picture.url
         return None
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Car
-from .serializers import CarSerializer
+from .serializers import CarSerializer, UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
@@ -19,7 +19,22 @@ class CarViewSet(viewsets.ModelViewSet):
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        # Создаем JWT токены для нового пользователя
+        refresh = RefreshToken.for_user(user)
+        
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'username': user.username
+        }, status=status.HTTP_201_CREATED)
     
 class FrontendAppView(TemplateView):
     template_name = "index.html"
